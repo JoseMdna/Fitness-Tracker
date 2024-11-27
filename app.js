dab5858a891362873135e208a35c882e5c82a490
JoseMdna <jose73001@gmail.com>
2024-11-26 18:14:50 -0800

    bug

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const morgan = require('morgan');
+const MongoStore = require('connect-mongo');
 const User = require('./models/User');
 const Activity = require('./models/Activity');
 const { isAuthenticated, restrictGuests } = require('./routes/middleware/authenticate');
@@ -14,7 +15,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
 
@@ -25,13 +26,15 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
     secret: process.env.SESSION_SECRET || 'secret', 
-    resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
     cookie: {
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true, 
-        maxAge: 1000 * 60 * 60 * 24,
-      },
+        maxAge: 1000 * 60 * 60 * 24
+    }
+})); // <-- Missing closing parenthesis added here
+
 app.use(async (req, res, next) => {
     if (req.session.userId) {
         try {
